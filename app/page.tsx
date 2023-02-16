c2ce9c2c81e2aabd9c6fbd24de6916a2e5f5285b
Sang Vo <sangvo235@gmail.com>
2023-02-16 16:45:38 +1100

    uupdated with db retrieval

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
 import Link from "next/link"
 
-export default function Home() {
+async function getPosts(){
+  const res = await fetch(`${process.env.BASE_URL}/api/getPosts`)
+  if(!res.ok){
+    console.log(res)
+  }
+  return res.json()
+}
+
+export default async function Home() {
+  const data : {id: number, title: string}[] = await getPosts()
+  // console.log(data)
+
   return (
     <main className="py-4 px-48">
-        <Link className="bg-teal-700 text-black font-medium py-2 px-4 rounded-md" href={"/dashboard"}>Go to the dashboard</Link>
+        <Link 
+          className="bg-teal-700 text-black font-medium py-2 px-4 rounded-md" 
+          href={"/dashboard"}
+          >
+          Go to the dashboard
+        </Link>
+        {data.map(post => (
+          <h1 className="text-lg py-6">{post.title}</h1>
+        ))}
     </main>
   )
 }
